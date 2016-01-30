1896a2fd8d1ec3a182a413f9387a4c9f37ce39de
Eugene Guzik <guzik.evgeny@gmail.com>
2016-01-30 23:24:56 +0200

    gcm complete

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -32,133 +32,143 @@ import com.google.android.gms.common.GoogleApiAvailability;
 
 public class MainActivity extends ActionBarActivity implements ForecastFragment.Callback {
 
-    private final String LOG_TAG = MainActivity.class.getSimpleName();
-    private static final String DETAILFRAGMENT_TAG = "DFTAG";
-    private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
-    public static final String SENT_TOKEN_TO_SERVER = "sentTokenToServer";
+  private final String LOG_TAG = MainActivity.class.getSimpleName();
+  private static final String DETAILFRAGMENT_TAG = "DFTAG";
+  private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+  public static final String SENT_TOKEN_TO_SERVER = "sentTokenToServer";
 
-    private boolean mTwoPane;
-    private String mLocation;
+  private boolean mTwoPane;
+  private String mLocation;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        mLocation = Utility.getPreferredLocation(this);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    mLocation = Utility.getPreferredLocation(this);
 
-        setContentView(R.layout.activity_main);
-        if (findViewById(R.id.weather_detail_container) != null) {
-            // The detail container view will be present only in the large-screen layouts
-            // (res/layout-sw600dp). If this view is present, then the activity should be
-            // in two-pane mode.
-            mTwoPane = true;
-            // In two-pane mode, show the detail view in this activity by
-            // adding or replacing the detail fragment using a
-            // fragment transaction.
-            if (savedInstanceState == null) {
-                getSupportFragmentManager().beginTransaction()
-                        .replace(R.id.weather_detail_container, new DetailFragment(), DETAILFRAGMENT_TAG)
-                        .commit();
-            }
-        } else {
-            mTwoPane = false;
-            getSupportActionBar().setElevation(0f);
-        }
-
-        ForecastFragment forecastFragment =  ((ForecastFragment)getSupportFragmentManager()
-                .findFragmentById(R.id.fragment_forecast));
-        forecastFragment.setUseTodayLayout(!mTwoPane);
-
-        SunshineSyncAdapter.initializeSyncAdapter(this);
-        if (!checkPlayServices()) {
-            // Because this is the initial creation of the app, we'll want to be certain we have
-            // a token. If we do not, then we will start the IntentService that will register this
-            // application with GCM.
-            SharedPreferences sharedPreferences =
-                    PreferenceManager.getDefaultSharedPreferences(this);
-            boolean sentToken = sharedPreferences.getBoolean(SENT_TOKEN_TO_SERVER, false);
-            if (!sentToken) {
-                Intent intent = new Intent(this, RegistrationIntentService.class);
-                startService(intent);
-            }
-        }
+    setContentView(R.layout.activity_main);
+    if (findViewById(R.id.weather_detail_container) != null) {
+      // The detail container view will be present only in the large-screen layouts
+      // (res/layout-sw600dp). If this view is present, then the activity should be
+      // in two-pane mode.
+      mTwoPane = true;
+      // In two-pane mode, show the detail view in this activity by
+      // adding or replacing the detail fragment using a
+      // fragment transaction.
+      if (savedInstanceState == null) {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.weather_detail_container, new DetailFragment(), DETAILFRAGMENT_TAG)
+                .commit();
+      }
+    } else {
+      mTwoPane = false;
+      getSupportActionBar().setElevation(0f);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+    ForecastFragment forecastFragment =  ((ForecastFragment)getSupportFragmentManager()
+            .findFragmentById(R.id.fragment_forecast));
+    forecastFragment.setUseTodayLayout(!mTwoPane);
+
+    SunshineSyncAdapter.initializeSyncAdapter(this);
+
+    // If Google Play Services is up to date, we'll want to register GCM. If it is not, we'll
+    // skip the registration and this device will not receive any downstream messages from
+    // our fake server. Because weather alerts are not a core feature of the app, this should
+    // not affect the behavior of the app, from a user perspective.
+    if (checkPlayServices()) {
+      // Because this is the initial creation of the app, we'll want to be certain we have
+      // a token. If we do not, then we will start the IntentService that will register this
+      // application with GCM.
+      SharedPreferences sharedPreferences =
+              PreferenceManager.getDefaultSharedPreferences(this);
+      boolean sentToken = sharedPreferences.getBoolean(SENT_TOKEN_TO_SERVER, false);
+      if (!sentToken) {
+        Intent intent = new Intent(this, RegistrationIntentService.class);
+        startService(intent);
+      }
+    }
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.main, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+    int id = item.getItemId();
+
+    //noinspection SimplifiableIfStatement
+    if (id == R.id.action_settings) {
+      startActivity(new Intent(this, SettingsActivity.class));
+      return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    return super.onOptionsItemSelected(item);
+  }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            startActivity(new Intent(this, SettingsActivity.class));
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+  @Override
+  protected void onResume() {
+    super.onResume();
+    String location = Utility.getPreferredLocation( this );
+    // update the location in our second pane using the fragment manager
+    if (location != null && !location.equals(mLocation)) {
+      ForecastFragment ff = (ForecastFragment)getSupportFragmentManager().findFragmentById(R.id.fragment_forecast);
+      if ( null != ff ) {
+        ff.onLocationChanged();
+      }
+      DetailFragment df = (DetailFragment)getSupportFragmentManager().findFragmentByTag(DETAILFRAGMENT_TAG);
+      if ( null != df ) {
+        df.onLocationChanged(location);
+      }
+      mLocation = location;
     }
+  }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        String location = Utility.getPreferredLocation(this);
-        // update the location in our second pane using the fragment manager
-            if (location != null && !location.equals(mLocation)) {
-            ForecastFragment ff = (ForecastFragment)getSupportFragmentManager().findFragmentById(R.id.fragment_forecast);
-            if ( null != ff ) {
-                ff.onLocationChanged();
-            }
-            DetailFragment df = (DetailFragment)getSupportFragmentManager().findFragmentByTag(DETAILFRAGMENT_TAG);
-            if ( null != df ) {
-                df.onLocationChanged(location);
-            }
-            mLocation = location;
-        }
+  @Override
+  public void onItemSelected(Uri contentUri) {
+    if (mTwoPane) {
+      // In two-pane mode, show the detail view in this activity by
+      // adding or replacing the detail fragment using a
+      // fragment transaction.
+      Bundle args = new Bundle();
+      args.putParcelable(DetailFragment.DETAIL_URI, contentUri);
+
+      DetailFragment fragment = new DetailFragment();
+      fragment.setArguments(args);
+
+      getSupportFragmentManager().beginTransaction()
+              .replace(R.id.weather_detail_container, fragment, DETAILFRAGMENT_TAG)
+              .commit();
+    } else {
+      Intent intent = new Intent(this, DetailActivity.class)
+              .setData(contentUri);
+      startActivity(intent);
     }
+  }
 
-    @Override
-    public void onItemSelected(Uri contentUri) {
-        if (mTwoPane) {
-            // In two-pane mode, show the detail view in this activity by
-            // adding or replacing the detail fragment using a
-            // fragment transaction.
-            Bundle args = new Bundle();
-            args.putParcelable(DetailFragment.DETAIL_URI, contentUri);
-
-            DetailFragment fragment = new DetailFragment();
-            fragment.setArguments(args);
-
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.weather_detail_container, fragment, DETAILFRAGMENT_TAG)
-                    .commit();
-        } else {
-            Intent intent = new Intent(this, DetailActivity.class)
-                    .setData(contentUri);
-            startActivity(intent);
-        }
+  /**
+   * Check the device to make sure it has the Google Play Services APK. If
+   * it doesn't, display a dialog that allows users to download the APK from
+   * the Google Play Store or enable it in the device's system settings.
+   */
+  private boolean checkPlayServices() {
+    GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
+    int resultCode = apiAvailability.isGooglePlayServicesAvailable(this);
+    if (resultCode != ConnectionResult.SUCCESS) {
+      if (apiAvailability.isUserResolvableError(resultCode)) {
+        apiAvailability.getErrorDialog(this, resultCode,
+                PLAY_SERVICES_RESOLUTION_REQUEST).show();
+      } else {
+        Log.i(LOG_TAG, "This device is not supported.");
+        finish();
+      }
+      return false;
     }
-
-    private boolean checkPlayServices() {
-        GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
-        int resultCode = apiAvailability.isGooglePlayServicesAvailable(this);
-        if (resultCode != ConnectionResult.SUCCESS) {
-            if (apiAvailability.isUserResolvableError(resultCode)) {
-                apiAvailability.getErrorDialog(this, resultCode,
-                        PLAY_SERVICES_RESOLUTION_REQUEST).show();
-            } else {
-                Log.i(LOG_TAG, "This device is not supported.");
-                finish();
-            }
-            return false;
-        }
-        return true;
-    }
+    return true;
+  }
 }
